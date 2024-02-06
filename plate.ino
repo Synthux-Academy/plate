ec01934c4997540604255852348bd7b64f939c1e
Vladislav Litvinenko <thatvlad@icloud.com>
2024-02-06 12:44:57 +0100

    Controls

--- plate.ino
+++ plate.ino
@@ -16,8 +16,8 @@ static Vox vox;
 static AKnob input_knob(A(S30));
 static AKnob decay_knob(A(S32));
 static AKnob freq_knob(A(S33));
-static AKnob knob_e(A(S34));
-static AKnob knob_f(A(S35));
+static AKnob delay_knob(A(S34));
+static AKnob wet_knob(A(S35));
 static AKnob left_fader(A(S36));
 static AKnob right_fader(A(S37));
 
@@ -30,7 +30,6 @@ static const int switch_2_b = D(S10);
 /////////////////////////// TOUCH  ///////////////////////////
 static synthux::simpletouch::Touch touch;
 
-float amp = 1.0;
 float decay = 1.f;
 
 void OnTouch(uint16_t pad) {
@@ -45,13 +44,14 @@ void OnRelease(uint16_t pad) {
 ////////////////////// DELAY LINE /////////////////////////////
 const int kDelayBufferLength = 96000; //2s @ 48KHz sample rate. delayLeftTime and kDelayRightTime can not be longer than this.
 DelayLine<float, kDelayBufferLength> delay_line;
-
-float delayTime = 12000; //250ms @ 48KHz sample rate. Should not be longer than kDelayBufferLength
-float delayFeedback = 0.8; // 0...1
+float delayFeedback = 0.7; // 0...1
 float delayWetMix = 0.33; // 0...1
 
 ///////////////////////////////////////////////////////////////
 ///////////////////// AUDIO CALLBACK //////////////////////////
+
+float inputGain = 1.f;
+
 void AudioCallback(float **in, float **out, size_t size) {
   float oscout = 0;
   float delayout = 0;
@@ -64,7 +64,7 @@ void AudioCallback(float **in, float **out, size_t size) {
     delay_line.Write((wet * delayFeedback) + dry);
     delayout = wet * delayWetMix + dry * (1 - delayWetMix);
     out[0][i] = oscout;
-    out[1][i] = delayout;
+    out[1][i] = delayout * inputGain;
   }
 }
 
@@ -91,7 +91,6 @@ void setup() {
   // initialize other modules here
   vox.Init(sample_rate);
   delay_line.Init();
-  delay_line.SetDelay(delayTime);
 
   // BEGIN CALLBACK
   DAISY.begin(AudioCallback);
@@ -104,6 +103,10 @@ void loop() {
   vox.SetDroning(digitalRead(drone_switch));
   vox.SetFreq(fmap(freq_knob.Process(), 30.0, 5000, Mapping::LOG));
   decay = fmap(decay_knob.Process(), 0.01, 3.0);
+
+  inputGain = fmap(input_knob.Process(), 0.2, 10.0, Mapping::EXP);
+  delayWetMix = wet_knob.Process();
+  delay_line.SetDelay(fmap(delay_knob.Process(), 480, 96000));
 
   //Examine whether particular pad (3 in this case) is being touched
   // auto is_pad_touched = touch.IsTouched(3); //0...11
